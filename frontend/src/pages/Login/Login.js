6fe182624999f97ee84460e5c555da2a0da7bbc8
Will <wbarker@alexinc.com>
2024-04-27 15:10:24 -0400

    Created base login, signup, and home page

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+import './Login.css';
+
+const Login = () => {
+    return ( <body> <LoginForm /> </body> );
+};
+
+export default Login;
